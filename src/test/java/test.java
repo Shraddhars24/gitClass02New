001ac6a4689e0f88a4aec4478b30c8dcc8b2eb0f
Shraddhars24 <shraddhars24@gmail.com>
2025-02-16 17:57:29 -0600

    first commit

---
+++ src/test/java/test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("This is the project Version 1");
+
+
+        //We are done with Version 1
+    }
+}
